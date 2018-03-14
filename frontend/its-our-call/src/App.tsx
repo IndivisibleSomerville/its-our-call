20af51af99536fbd181d6c9ab9bc2871f743e784
Andrew Tremblay <gloriousrocketship@gmail.com>
2018-03-14 18:18:41 -0400

    adding router for Home page

--- frontend/its-our-call/src/App.tsx
+++ frontend/its-our-call/src/App.tsx
@@ -1,20 +1,18 @@
 import * as React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import * as pages from './pages';
 import './App.css';
-
-const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact={true} path="/" component={pages.Home}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ frontend/its-our-call/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+interface HomeProps { }
+
+interface HomeState { }
+
+class Home extends React.Component<HomeProps, HomeState> {
+  render() {
+    return (
+      <div className="Page Home">
+        Home Page
+      </div>
+    );
+  }
+}
+
+export default Home;
