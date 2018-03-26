bdbbb3df61428baba1f49819996b637e0cd2d617
Andrew Tremblay <gloriousrocketship@gmail.com>
2018-03-26 12:49:34 -0400

    adding placeholder page for individual issue view (#10) for the issue list (#7)

--- frontend/its-our-call/src/App.tsx
+++ frontend/its-our-call/src/App.tsx
@@ -14,6 +14,7 @@ class App extends React.Component {
           <Switch>
             <Route exact={true} path="/" component={pages.Home}/>
             <Route exact={true} path="/issues" component={pages.IssueList}/>
+            <Route exact={true} path="/issues/:id" component={pages.IssueView}/>
           </Switch>
         </div>
       </BrowserRouter>
--- frontend/its-our-call/src/pages/index.tsx
+++ frontend/its-our-call/src/pages/index.tsx
@@ -1,3 +1,4 @@
 export { default as Home } from './Home';
 
 export { default as IssueList } from './IssueList';
+export { default as IssueView } from './IssueView';
