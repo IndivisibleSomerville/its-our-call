dc1a962edf9dbb9a98401080488e6201c2d163e9
Andrew Tremblay <gloriousrocketship@gmail.com>
2018-04-11 10:49:57 -0400

    making the bookmark star toggleable (api call is still a todo)

--- frontend/its-our-call/src/components/BookmarkStar.tsx
+++ frontend/its-our-call/src/components/BookmarkStar.tsx
@@ -1,20 +1,47 @@
 import * as React from 'react';
 import './BookmarkStar.css';
 
+import { IoIosStarOutline, IoIosStar } from 'react-icons/lib/io/';
+
 interface BookmarkStarProps {
   isHidden?: boolean;
   isBookmarked?: boolean;
   wasPressed?: string;
 }
 
-class BookmarkStar extends React.Component<BookmarkStarProps> {
+interface BookmarkStarState {
+  isBookmarked: boolean;
+}
+
+class BookmarkStar extends React.Component<BookmarkStarProps, BookmarkStarState> {
+  constructor(props: BookmarkStarProps) {
+    super(props);
+    this.state = {
+      isBookmarked: this.props.isBookmarked === true,
+    };
+    this.toggleBookmark = this.toggleBookmark.bind(this);
+  }
+  toggleBookmark() {
+    this.setState({isBookmarked: !this.state.isBookmarked});
+    // TODO: prompt other ui callbacks
+  }
+
   render() {
     if (this.props.isHidden) {
       return null;
     }
+    let starIcon = (<IoIosStarOutline />);
+    let bookmarkClass = 'unchecked';
+    if (this.state.isBookmarked) {
+      starIcon = (<IoIosStar />);
+      bookmarkClass = 'checked';
+    }
     return (
-      <div className="BookmarkStar">
-        ⋆
+      <div
+        className={'BookmarkStar ' + bookmarkClass}
+        onClick={this.toggleBookmark}
+      >
+        {starIcon}
       </div>
     );
   }
