212352b393c918d58131e3b000c6a9b75edb20fb
Andrew Tremblay <gloriousrocketship@gmail.com>
2018-04-18 03:05:36 -0400

    adding state svgs to the legislator badge (close #34)

--- frontend/its-our-call/src/components/LegislatorBadge.tsx
+++ frontend/its-our-call/src/components/LegislatorBadge.tsx
@@ -1,22 +1,32 @@
 import * as React from 'react';
-import './LegislatorRow.css';
+import './LegislatorBadge.css';
+
+// requires embedded svgs in the main body
+
+export type StateAbbreviation = 'AK' | 'AL' | 'AR' | 'AZ' | 'CA' | 'CO' | 'CT' | 'DC' | 'DE' | 'FL'
+  | 'GA' | 'HI' | 'IA' | 'ID' | 'IL' | 'IN' | 'KS' | 'KY' | 'LA' | 'MA' | 'MD' | 'ME' | 'MI' | 'MN'
+  | 'MO' | 'MS' | 'MT' | 'NC' | 'ND' | 'NE' | 'NH' | 'NJ' | 'NM' | 'NV' | 'NY' | 'OH' | 'OK' | 'OR'
+  | 'PA' | 'RI' | 'SC' | 'SD' | 'TN' | 'TX' | 'UT' | 'VA' | 'VT' | 'WA' | 'WI' | 'WV' | 'WY';
 
 interface LegislatorBadgeProps {
-  /** Supported values: 'dem', 'repub', undefined */
-  type?: string;
+  type: string;
+  state: string | StateAbbreviation;
 }
 
 class LegislatorBadge extends React.Component<LegislatorBadgeProps> {
   render() {
-    let undefinedPartyColor = '⚪️';
+    let undefinedPartyColor = '';
     if (this.props.type === 'dem') {
-      undefinedPartyColor = '🔵';
+      undefinedPartyColor = 'blue';
     } else if (this.props.type === 'repub') {
-      undefinedPartyColor = '🔴';
+      undefinedPartyColor = 'red';
     }
     return (
-      <div className="LegislatorBadge">
-        {undefinedPartyColor}
+      // tslint:disable:jsx-self-close
+      <div className={'LegislatorBadge ' + undefinedPartyColor}>
+        <svg className={'state-icon state-icon-' + this.props.state}>
+          <use xlinkHref={'#icon-state-' + this.props.state}></use>
+        </svg>
       </div>
     );
   }
