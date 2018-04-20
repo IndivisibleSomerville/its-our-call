eefda144365c6ce65be5553f86c3ac6030bf00cb
Andrew Tremblay <gloriousrocketship@gmail.com>
2018-04-20 02:40:29 -0400

    adding json typing module and adding state data to use with the map svg

---
+++ frontend/its-our-call/src/data/usa-states-dimensions/index.tsx
@@ -0,0 +1,2 @@
+import * as stateData from './usa-states-dimensions.json';
+export default stateData;
---
+++ frontend/its-our-call/src/typings.d.ts
@@ -0,0 +1,5 @@
+declare module '*.json' {
+    // tslint:disable-next-line:no-any
+    const value: any;
+    export default value;
+}
