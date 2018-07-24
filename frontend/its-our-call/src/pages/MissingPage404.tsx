8f06c969d4dfb02c5a10af08998c709bd48a7654
Kevin Scott <kevin@hitheory.com>
2018-07-23 20:00:02 -0400

    Update styles

--- frontend/its-our-call/src/pages/MissingPage404.tsx
+++ frontend/its-our-call/src/pages/MissingPage404.tsx
@@ -3,18 +3,14 @@ import * as React from 'react';
 import './Page.css';
 import './MissingPage404.css';
 
-import { Footer } from '../components';
-
 interface MissingPage404Props { }
 
 const MissingPage404: React.SFC<MissingPage404Props> = () => (
   <div className="Page MissingPage404">
     <div className="full-height scrollable">
       <div className="content">
-        <div className="copy">
-          <h1>404</h1>
-        </div>
-        <Footer />
+        <h1>404</h1>
+        <p>Sorry! The page you're looking for cannot be found.</p>
       </div>
     </div>
   </div>
